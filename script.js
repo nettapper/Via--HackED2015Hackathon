09f24eeaa7b0e503c3e822caf1bf145b24f4c613
Conner D <connerdunn7399@gmail.com>
2015-01-31 13:42:24 -0700

    working paragraph example

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Welcome!");
+// Paragraph Example
+d3.select("body").append("p").text("New paragraph!");
+var data = [1,2,3,4,5];
+d3.select("body").selectAll("p").data(data).enter().append("p")
+    .text(function(d){return d;})
+    .style("color", function(d) {
+	if (d <= 3) {   //Threshold of 15
+	    return "red";
+	} else {
+	    return "purple";
+	}
+    });
+
