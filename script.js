964ad3315b4c69880cb0b820aea11cfd22cc7cb2
Conner D <connerdunn7399@gmail.com>
2015-01-31 14:33:04 -0700

    gen svg circles of various sizes

--- script.js
+++ script.js
@@ -13,9 +13,23 @@
 
 // Bar Graph Example
 //var dataset = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19, 14, 11, 22, 29, 11, 13, 12, 17, 18, 10, 24, 18, 25, 9, 3 ];
-var dataset = [];                        //Initialize empty array
-for (var i = 0; i < 25; i++) {           //Loop 25 times
-    var newNumber = Math.round(Math.random() * 30);  //New random number (0-30)
-    dataset.push(newNumber);             //Add new number to array
-}
-d3.select("body").selectAll("div").data(dataset).enter().append("div").attr("class", "bar").style("height", function(d){return d*5 + "px"});
+// var dataset = [];                        //Initialize empty array
+// for (var i = 0; i < 25; i++) {           //Loop 25 times
+    // var newNumber = Math.round(Math.random() * 30);  //New random number (0-30)
+    // dataset.push(newNumber);             //Add new number to array
+// }
+// d3.select("body").selectAll("div").data(dataset).enter().append("div").attr("class", "bar").style("height", function(d){return d*5 + "px"});
+
+// SVG shapes
+var dataset = [5, 10, 15, 20, 25];
+var w = 500;
+var h = 50;
+var svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+var circles = svg.selectAll("svg").data(dataset).enter().append("circle");
+circles.attr("cx", function(d, i) {
+            return (i * 50) + 25;
+        })
+       .attr("cy", h/2)
+       .attr("r", function(d) {
+            return d;
+       });
