8dbb2d27bf6fd7807106a63b3343fefb3e9cdef7
Conner D <connerdunn7399@gmail.com>
2015-02-10 19:51:28 -0700

    preventevent, links don't work

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // My script
 $(document).ready(function(){
-    alert("hello");
+    $("a").click(function(event){
+	alert("baby come back!");
+	event.preventDefault();
+	console.log("i won't let you leave!!");
+    });
 });
